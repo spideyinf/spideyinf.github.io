039d6913da17eb96cfd8f7e35e4373fd3b953eb0
spideyinf <spidey.inf@gmail.com>
2017-06-22 11:54:37 +0700

    Added validation

--- Register-form/main.js
+++ Register-form/main.js
@@ -23,6 +23,25 @@ function validatePassword(){
 pass.onchange = validatePassword;
 confirmPass.onkeyup = validatePassword;
 
+//Validation
 
+$('#form-register').on('submit', function() {
+	var isValid = true;
+	if ($('#fullname').val().trim() == '') {
+		$('#fullname').next('span').text('Fullname is empty');
+		// $('#error').text('Fullname is empty'); //Call directly
+		isValid = false;
+	} else {
+		$('#fullname').next('span').text('');
+	}
 
+	if ($('#email').val().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/) == null) {
+		$('#email').next('span').text('Email is invalid');
+		isValid = false;
+	} else {
+		$('#email').next('span').text('');
+	}
+
+	return isValid;
+});
 
