bf63982617014fea0655579904b633e4d11b4db1
spideyinf <spidey.inf@gmail.com>
2017-06-28 17:17:19 +0700

    Update full Kanban board features

--- 06-26-Kanban-board/main.js
+++ 06-26-Kanban-board/main.js
@@ -1,5 +1,7 @@
 ﻿var COLUMN_TYPE = ['todo', 'doing', 'done'];
 
+var LIMIT = {'todo': 5, 'doing': 3, 'done': 100};
+
 var DB = {
 	getData: function () {
 		if (typeof(Storage) !== "undefined") {
@@ -16,12 +18,10 @@ var DB = {
 	},
 	setData: function (data) {
 		localStorage.setItem('list', JSON.stringify(data));
-
 	}
 };
 
 var list = DB.getData();
-
 
 var app = {
 	newTask: function(e, type, input) {
@@ -31,11 +31,26 @@ var app = {
 		if (event.keyCode === 13 && taskName.trim() !== '') {
 			//Add local storage info to the list
 			if (!list[type]) list[type] = [];
+
 			list[type].push(taskName);
+
+			//Limit task for each column
+			if (list[type].length > LIMIT[type]) {
+				
+				//Display modal
+				$('.limit-count').text(LIMIT[type]);
+				$('.limit-list').text(type);
+				$('#modal-limit').modal('show');	
+				
+				list[type].pop(taskName);
+				return $(input).val('');
+			}
+
 			DB.setData(list);
 
 			//Update DOM - add task to the list
 			this.addTaskToList(type, taskName);
+			$('#' + type).prev('span').text(list[type].length);
 			//Reset input
 			$(input).val('');
 		}
@@ -46,18 +61,19 @@ var app = {
 		$('#' + type).append(item);
 	},
 
-	deleteTask: function (span) {
+	deleteTask: function(span) {
 		var modal = $('#modal-confirm');
 		var item = $(span).parent();
 		var btn = $('#btn-delete');
 		
 		//Trigger the modal when clicking in span
-		$('span').click(function () {
+		$('span').on('click', function () {
   			modal.modal('show');
 		});
 
 		btn.off('click');
-		//Remove task at UI and local storage info
+
+		//Remove task at UI and local storage data
 		btn.on('click', function () {
 			var columnType = item.parent().attr('id');
 			var itemPosition = $('#' + columnType + ' .list-group-item').index(item);
@@ -65,16 +81,10 @@ var app = {
 			DB.setData(list);
 			item.remove();
 			modal.modal('hide');
+			$('#' + columnType).prev('span').text(list[columnType].length);
 		});
+	},
 
-	}
-
-	// addTrashIcon() {
-	// 	var listItem = $('.list-group-item');
-	// 	var trashIcon = '<span class="glyphicon glyphicon-trash pull-right"></span>';
-	// 	listItem.append(trashIcon);
-	// 	return listItem;
-	// }
 };
 
 
@@ -84,6 +94,7 @@ $(function() { //Initiate sorttable for .sort-list - A jQueryUI feature + Add lo
     	var columnType = list[type] || [];
     	columnType.forEach(function(taskName) {
     		app.addTaskToList(type, taskName);
+    		$('#' + type).prev('span').text(list[type].length);
     	})
     });
 
@@ -93,10 +104,45 @@ $(function() { //Initiate sorttable for .sort-list - A jQueryUI feature + Add lo
       start: function (event, ui) {
       	//Add style class
       	$(ui.item[0]).addClass('dragging');
+
+      	ui.item.oldColumnType = ui.item.parent().attr('id');
+      	ui.item.oldItemPosition = ui.item.index();
       },
       stop: function (event, ui) {
       	//Remove style class
       	$(ui.item[0]).removeClass('dragging');
+
+      	var item = ui.item;
+      	var oldColumnType = item.oldColumnType;
+      	var oldItemPosition = item.oldItemPosition;
+      	var newColumnType = item.parent().attr('id');
+      	var newItemPosition = item.index();
+
+      	//Remove item from old position
+      	list[oldColumnType].splice(oldItemPosition, 1);
+
+      	//Add it to new position
+      	list[newColumnType].splice(newItemPosition, 0, item[0].innerText);
+
+      	//Limit task of target list
+      	if (list[newColumnType].length > LIMIT[newColumnType]) {
+      		
+      		//Display Modal
+      		$('.limit-count').text(LIMIT[newColumnType]);
+      		$('.limit-list').text(newColumnType);
+      		$('#modal-limit').modal('show');
+
+      		//Withdraw all the actions
+      		$('.sort-list').sortable('cancel');
+      		list[newColumnType].splice(newItemPosition, 1);
+      		list[oldColumnType].splice(oldItemPosition, 0, item[0].innerText);
+      	}
+
+      	//Update badge
+      	$('#' + oldColumnType).prev('span').text(list[oldColumnType].length);
+      	$('#' + newColumnType).prev('span').text(list[newColumnType].length);
+      	//Store data to local storage
+      	DB.setData(list);
       }
     });
   });
