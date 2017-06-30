c26b8ca223571eab59f28b5ca1178e20fdd98c7d
spideyinf <spidey.inf@gmail.com>
2017-07-01 01:25:19 +0700

    Add audio and logic

--- 06-29-Memory-card/main.js
+++ 06-29-Memory-card/main.js
@@ -1,8 +1,10 @@
 ﻿var cardFront = [];
 	//Create card front
-	for (var i = 1; i < 6; i ++) {
+	for (var i = 1; i < 10; i ++) {
 		cardFront.push('images/f' + [i] +'.png');
 	};
+
+var background = ['background1.jpg', 'background2.jpeg', 'background3.jpeg', 'background4.jpg'];
 
 var cardBack = 'images/back.jpg'
 var current = null;
@@ -27,7 +29,14 @@ function shuffle(arr) {
 	return arr;
 }
 
-$(function () {
+//Prepare to battle - Background
+$(function() {
+	background = shuffle(background);
+	$('body').css('background-image', 'url(images/' + background[0] + ')');
+})
+
+//Prepare the field
+$(function() {
 	cardFront = cardFront.concat(cardFront);
 	cardFront = shuffle(cardFront);
 
@@ -44,26 +53,31 @@ $(function () {
 
 function flip(card) {
 	$(card).toggleClass('flipped');
+	$(card).css('pointer-events', 'none');
 	if (!current) {
 		current = $(card);
 	} else {
+		$('.card-hs').css('pointer-events', 'none');
 		if (current.attr('data-name') != $(card).attr('data-name')) {
-			
 			//Wrong cards
+
 			setTimeout(function() {
+				document.getElementById('wrong').play();
 				current.toggleClass('flipped');
 				$(card).toggleClass('flipped');
 				current = null;
-			}, 800);
+				$('.card-hs').css('pointer-events', 'auto');
+			}, 500);
 
 		//Correct
 		} else {
-
 			setTimeout(function() {
+			document.getElementById('correct').play();
 			$(card).css('opacity', '0');
 			current.css('opacity', '0');		
-			current = null;		
-			}, 300);
+			current = null;	
+			$('.card-hs').css('pointer-events', 'auto');	
+			}, 400);
 
 		}
 	}
