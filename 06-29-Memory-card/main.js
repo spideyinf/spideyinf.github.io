b94fe5fc391138467ba6cd10b061731bd6d262c0
spideyinf <spidey.inf@gmail.com>
2017-07-04 16:09:34 +0700

    Complete

--- 06-29-Memory-card/main.js
+++ 06-29-Memory-card/main.js
@@ -1,13 +1,32 @@
-﻿var cardFront = [];
-	//Create card front
-	for (var i = 1; i < 10; i ++) {
-		cardFront.push('images/f' + [i] +'.png');
-	};
+﻿var mode = {
+	'normal':{'pairs': 6, 'time': 40}, 
+	'hard':{'pairs': 9, 'time': 38}
+};
 
 var background = ['background1.jpg', 'background2.jpeg', 'background3.jpeg', 'background4.jpg'];
 
 var cardBack = 'images/back.jpg'
 var current = null;
+var count = 0;
+var progressPercent = 100;
+var soundSource = {
+	soundBackground: 'sounds/Main_Title.ogg',
+	soundFlip: 'sounds/draw_card_1.ogg',
+	soundFlipWrong: 'sounds/wrong.ogg',
+	soundFlipCorrect: 'sounds/correct.ogg',
+	soundDefeated: 'sounds/defeat_jingle.ogg',
+	soundVictory1: 'sounds/victory_screen_start.ogg',
+	soundVictory2: 'sounds/victory_jingle.ogg'	
+};
+
+function soundEffect(source) {
+	var sound = document.createElement("audio");
+	sound.src = source;
+	sound.addEventListener('ended', function() {
+		this.classList.remove();
+	}, false);
+	return sound;
+};
 
 function shuffle(arr) {
 	var currentIndex = arr.length;
@@ -28,31 +47,88 @@ function shuffle(arr) {
 	}
 	return arr;
 }
-
 //Prepare to battle - Background
 $(function() {
 	background = shuffle(background);
 	$('body').css('background-image', 'url(images/' + background[0] + ')');
 })
 
-//Prepare the field
-$(function() {
+//Start game
+$('#startGame').modal({
+	show: true,
+	backdrop: 'static',
+	keyboard: false
+});
+
+$('.btn-mode').on('click', function(event) {
+	event.preventDefault();
+	var timeMode = mode[$(this).attr('value')].time;
+	var cardMode = mode[$(this).attr('value')].pairs;
+
+var cardFront = [];
+	//Create card front
+	for (var i = 1; i <= cardMode; i ++) {
+		cardFront.push('images/f' + [i] +'.png');
+	};
+
+var amount = 100/timeMode;
+console.log(amount);
+	//Prepare the field
+	var counterBack = setInterval(function() {
+		progressPercent -= amount;
+		if (progressPercent > 25) {
+			$('.progress-bar').css('width', progressPercent + '%');
+		} else if (progressPercent <=25) {
+			$('.progress-bar').removeClass('progress-bar-warning');
+			$('.progress-bar').addClass('progress-bar-danger')
+			$('.progress-bar').css({
+				'transition': 'width 1s linear',
+				'width': progressPercent + '%',
+				'transition-duration': '1000ms'
+			});
+		} else {
+			clearInterval(counterBack);
+		}	
+	}, 1000);
+
+
 	cardFront = cardFront.concat(cardFront);
 	cardFront = shuffle(cardFront);
 
 	for (var i = 0; i < cardFront.length; i ++) {
 		var cardHS = '<div class="col-xs-2"><div class="card-hs" data-name="' + cardFront[i] + '" onclick="flip(this)"><div class="front"><img class="img-responsive" src=' + cardFront[i] + '></div><div class="back"><img class="img-responsive" src=' + cardBack + '></div></div></div>';
-		$('.row').append(cardHS);
+		$('.container .row').append(cardHS);
 	};
 
-	// var rows = $('.container-fluid > .col-xs-2');
-	// for (var i = 0; i < rows.length; i += 6) {	
-	// 	rows.slice(i, i+6).wrapAll('<div class="row row-card"></div>');
-	// }
-})
+	var run = setInterval(function() {
+		timeMode --;
+		console.log(timeMode);
+		//Victory
+		if (count == cardMode) {
+			clearInterval(run);
+			clearInterval(counterBack);
+			$('.modal-victory').modal({show: true, backdrop: 'static', keyboard: false});
+			document.getElementById('soundBackground').pause();
+			soundEffect(soundSource.soundVictory1).play();
+			soundEffect(soundSource.soundVictory2).play();
+			$('.card-hs').css('pointer-events', 'none');
+		}
+		//Time out
+		if (timeMode == 0) {
+			//Game is over
+			clearInterval(run);
+			clearInterval(counterBack);
+			$('.modal-defeated').modal({show: true, backdrop: 'static', keyboard: false});
+			document.getElementById('soundBackground').pause();
+			soundEffect(soundSource.soundDefeated).play();
+		}
+	}, 1000);
+});
+
 
 function flip(card) {
 	$(card).toggleClass('flipped');
+	soundEffect(soundSource.soundFlip).play();
 	$(card).css('pointer-events', 'none');
 	if (!current) {
 		current = $(card);
@@ -62,28 +138,42 @@ function flip(card) {
 			//Wrong cards
 
 			setTimeout(function() {
-				document.getElementById('wrong').play();
+				soundEffect(soundSource.soundFlipWrong).play();
 				current.toggleClass('flipped');
 				$(card).toggleClass('flipped');
 				current = null;
+				$('.card-hs.flipped').css('pointer-events', 'none');
 				$('.card-hs').css('pointer-events', 'auto');
 			}, 500);
 
 		//Correct
 		} else {
 			setTimeout(function() {
-			document.getElementById('correct').play();
+			soundEffect(soundSource.soundFlipCorrect).play();
 			$(card).css('opacity', '0');
 			current.css('opacity', '0');		
 			current = null;	
-			$('.card-hs').css('pointer-events', 'auto');	
-			}, 400);
+			count ++;
+			$('.card-hs').css('pointer-events', 'auto');
+			$('.card-hs.flipped').css('pointer-events', 'none');	
+			}, 500);
 
 		}
 	}
 }
 
-$(function() {
+function replay() {
+	window.location.reload();
+}
 
-});
+function remove() {
+	$('.container').remove();
+	$('.reward-container').remove();
+}
+
+function showReward() {
+	$('.reward').css('display', 'block');
+}
+
+
 
