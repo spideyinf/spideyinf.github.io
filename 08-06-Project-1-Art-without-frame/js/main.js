429c7c7eb73913b3df93775dc11abd6c9b7b7831
spideyinf <spidey.inf@gmail.com>
2017-08-15 17:53:57 +0700

    user page update

--- 08-06-Project-1-Art-without-frame/js/main.js
+++ 08-06-Project-1-Art-without-frame/js/main.js
@@ -1,4 +1,22 @@
-﻿// Home -  Flex panel galery
+﻿//Navbar control
+
+$(document).ready(function(){       
+                var scroll_start = 0;
+                var startchange = $('#startchange');
+                var offset = startchange.offset();
+                    if (startchange.length){
+                $(document).scroll(function() { 
+                    scroll_start = $(this).scrollTop();
+                    if(scroll_start > offset.top) {
+                          $(".navbar-default").css('background-color', '#ffffff');
+                       } else {
+                          $('.navbar-default').css('background-color', 'transparent');
+                       }
+                   });
+                    }
+                });
+
+// Home -  Flex panel galery
 
 const panels = document.querySelectorAll('.home-art-panels__panel');
 
@@ -17,167 +35,16 @@ panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
 $('.selectpicker').selectpicker({
   });
 
-//About - Google map API
+//Dropdown kickoff
 
-var map;
-function initialize() {
-	var mapOptions = {
-		zoom: 9,
-		center: new google.maps.LatLng(50.744365542764285, -114.13913259277342),
-		styles: 
-		[
-		    {
-		        "featureType": "water",
-		        "elementType": "all",
-		        "stylers": [
-		            {
-		                "color": "#3b5998"
-		            }
-		        ]
-		    },
-		    {
-		        "featureType": "administrative.province",
-		        "elementType": "all",
-		        "stylers": [
-		            {
-		                "visibility": "off"
-		            }
-		        ]
-		    },
-		    {
-		        "featureType": "all",
-		        "elementType": "all",
-		        "stylers": [
-		            {
-		                "hue": "#3b5998"
-		            },
-		            {
-		                "saturation": -22
-		            }
-		        ]
-		    },
-		    {
-		        "featureType": "landscape",
-		        "elementType": "all",
-		        "stylers": [
-		            {
-		                "visibility": "on"
-		            },
-		            {
-		                "color": "#f7f7f7"
-		            },
-		            {
-		                "saturation": 10
-		            },
-		            {
-		                "lightness": 76
-		            }
-		        ]
-		    },
-		    {
-		        "featureType": "landscape.natural",
-		        "elementType": "all",
-		        "stylers": [
-		            {
-		                "color": "#f7f7f7"
-		            }
-		        ]
-		    },
-		    {
-		        "featureType": "road.highway",
-		        "elementType": "all",
-		        "stylers": [
-		            {
-		                "color": "#8b9dc3"
-		            }
-		        ]
-		    },
-		    {
-		        "featureType": "administrative.country",
-		        "elementType": "geometry.stroke",
-		        "stylers": [
-		            {
-		                "visibility": "simplified"
-		            },
-		            {
-		                "color": "#3b5998"
-		            }
-		        ]
-		    },
-		    {
-		        "featureType": "road.highway",
-		        "elementType": "all",
-		        "stylers": [
-		            {
-		                "visibility": "on"
-		            },
-		            {
-		                "color": "#8b9dc3"
-		            }
-		        ]
-		    },
-		    {
-		        "featureType": "road.highway",
-		        "elementType": "all",
-		        "stylers": [
-		            {
-		                "visibility": "simplified"
-		            },
-		            {
-		                "color": "#8b9dc3"
-		            }
-		        ]
-		    },
-		    {
-		        "featureType": "transit.line",
-		        "elementType": "all",
-		        "stylers": [
-		            {
-		                "invert_lightness": false
-		            },
-		            {
-		                "color": "#ffffff"
-		            },
-		            {
-		                "weight": 0.43
-		            }
-		        ]
-		    },
-		    {
-		        "featureType": "road.highway",
-		        "elementType": "labels.icon",
-		        "stylers": [
-		            {
-		                "visibility": "off"
-		            }
-		        ]
-		    },
-		    {
-		        "featureType": "road.local",
-		        "elementType": "geometry.fill",
-		        "stylers": [
-		            {
-		                "color": "#8b9dc3"
-		            }
-		        ]
-		    },
-		    {
-		        "featureType": "administrative",
-		        "elementType": "labels.icon",
-		        "stylers": [
-		            {
-		                "visibility": "on"
-		            },
-		            {
-		                "color": "#3b5998"
-		            }
-		        ]
-		    }
-		]
-	};
-	map = new google.maps.Map(document.getElementById('contact-us__map'), mapOptions);
-}
+$(document).ready(function () {
+    $('.dropdown-toggle').dropdown();
+});
 
-google.maps.event.addDomListener(window, 'load', initialize);
+ $(document).ready(function () { 
+    $(".selectpicker").selectpicker();
+    $(".bootstrap-select").click(function () {
+         $(this).addClass("open");
+    });
+  });
 
-//End of About - Google map API
