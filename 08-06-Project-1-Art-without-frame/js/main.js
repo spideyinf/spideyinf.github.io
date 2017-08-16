77f8489f25ad3bcf97b57186e1595472dd19d952
spideyinf <spidey.inf@gmail.com>
2017-08-17 00:26:53 +0700

    Day 7 still survival

--- 08-06-Project-1-Art-without-frame/js/main.js
+++ 08-06-Project-1-Art-without-frame/js/main.js
@@ -8,9 +8,13 @@ $(document).ready(function(){
                 $(document).scroll(function() { 
                     scroll_start = $(this).scrollTop();
                     if(scroll_start > offset.top) {
-                          $(".navbar-default").css('background-color', '#ffffff');
+                          $(".navbar-default").css('background-color', 'white');
+                          $('.logo-brand').attr('src', 'images/logo/logo2.png');
+                          $('.nav-link, .nav-link__icon, .dropdown-toggle, .dropdown-toggle__icon, .caret').css('color', 'gray');
                        } else {
                           $('.navbar-default').css('background-color', 'transparent');
+                          $('.logo-brand').attr('src', 'images/logo/logo.png');
+                          $('.nav-link, .nav-link__icon, .dropdown-toggle, .dropdown-toggle__icon, .caret').css('color', 'white');
                        }
                    });
                     }
@@ -47,4 +51,32 @@ $(document).ready(function () {
          $(this).addClass("open");
     });
   });
+
+ //Carousel full screen function
+
+var $item = $('.carousel .item'); 
+var $wHeight = $(window).height();
+$item.eq(0).addClass('active');
+$item.height($wHeight); 
+$item.addClass('full-screen');
+
+$('.carousel img').each(function() {
+  var $src = $(this).attr('src');
+  var $color = $(this).attr('data-color');
+  $(this).parent().css({
+    'background-image' : 'url(' + $src + ')',
+    'background-color' : $color
+  });
+  $(this).remove();
+});
+
+$(window).on('resize', function (){
+  $wHeight = $(window).height();
+  $item.height($wHeight);
+});
+
+$('.carousel').carousel({
+  interval: 6000,
+  pause: "false"
+});
 
