a52b3731914399665caf8eb2e376d7e657daf229
spideyinf <spidey.inf@gmail.com>
2017-08-20 01:11:26 +0700

    Hardcore and more...

--- 08-06-Project-1-Art-without-frame/js/main.js
+++ 08-06-Project-1-Art-without-frame/js/main.js
@@ -123,3 +123,55 @@ $('.carousel').carousel({
   interval: 8000,
   pause: true,
 });
+
+//-------------------------------------//
+// init Masonry
+
+var $grid = $('.grid').masonry({
+  itemSelector: 'none', // select none at first
+  columnWidth: '.grid__col-sizer',
+  gutter: '.grid__gutter-sizer',
+  percentPosition: true,
+  stagger: 30,
+  // nicer reveal transition
+  visibleStyle: { transform: 'translateY(0)', opacity: 1 },
+  hiddenStyle: { transform: 'translateY(100px)', opacity: 0 },
+});
+
+// get Masonry instance
+var msnry = $grid.data('masonry');
+
+// initial items reveal
+$grid.imagesLoaded( function() {
+  $grid.removeClass('are-images-unloaded');
+  $grid.masonry( 'option', { itemSelector: '.grid__item' });
+  var $items = $grid.find('.grid__item');
+  $grid.masonry( 'appended', $items );
+});
+
+//-------------------------------------//
+// hack CodePen to load pens as pages
+
+// var nextPenSlugs = [
+//   '202252c2f5f192688dada252913ccf13',
+//   'a308f05af22690139e9a2bc655bfe3ee',
+//   '6c9ff23039157ee37b3ab982245eef28',
+// ];
+
+// function getPenPath() {
+//   var slug = nextPenSlugs[ this.loadCount ];
+//   if ( slug ) {
+//     return 'https://s.codepen.io/desandro/debug/' + slug;
+//   }
+// }
+
+//-------------------------------------//
+// init Infinte Scroll
+
+$grid.infiniteScroll({
+  path: getPenPath,
+  append: '.grid__item',
+  outlayer: msnry,
+  status: '.page-load-status',
+});
+
