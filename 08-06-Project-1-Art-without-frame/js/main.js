2dfd9fb0a40b3625e792936bc6ba0c5144f261e4
spideyinf <spidey.inf@gmail.com>
2017-08-21 00:20:00 +0700

    masonry and infinite scroll

--- 08-06-Project-1-Art-without-frame/js/main.js
+++ 08-06-Project-1-Art-without-frame/js/main.js
@@ -152,18 +152,18 @@ $grid.imagesLoaded( function() {
 //-------------------------------------//
 // hack CodePen to load pens as pages
 
-// var nextPenSlugs = [
-//   '202252c2f5f192688dada252913ccf13',
-//   'a308f05af22690139e9a2bc655bfe3ee',
-//   '6c9ff23039157ee37b3ab982245eef28',
-// ];
+var nextPenSlugs = [
+  '202252c2f5f192688dada252913ccf13',
+  'a308f05af22690139e9a2bc655bfe3ee',
+  '6c9ff23039157ee37b3ab982245eef28',
+];
 
-// function getPenPath() {
-//   var slug = nextPenSlugs[ this.loadCount ];
-//   if ( slug ) {
-//     return 'https://s.codepen.io/desandro/debug/' + slug;
-//   }
-// }
+function getPenPath() {
+  var slug = nextPenSlugs[ this.loadCount ];
+  if ( slug ) {
+    return 'https://s.codepen.io/desandro/debug/' + slug;
+  }
+}
 
 //-------------------------------------//
 // init Infinte Scroll
