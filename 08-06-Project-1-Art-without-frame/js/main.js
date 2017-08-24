eef9192037779bc05c1f6c82165fb184b33e3416
spideyinf <spidey.inf@gmail.com>
2017-08-25 00:05:52 +0700

    stay hungry, stay foolish

--- 08-06-Project-1-Art-without-frame/js/main.js
+++ 08-06-Project-1-Art-without-frame/js/main.js
@@ -11,10 +11,10 @@ $(document).ready(function(){
         scroll_start = $(this).scrollTop();
           if(scroll_start > offset.top) {
             $(".navbar-default").css('background-color', 'rgba(15, 15, 15, 0.8)');
-              $('.nav-link, .nav-link__icon, .dropdown-toggle, .dropdown-toggle__icon, .caret').css('color', 'white');
+              $('.nav-link, .nav-link__icon, .dropdown-toggle, .dropdown-toggle__icon, .caret.top').css('color', 'white');
             } else {
               $('.navbar-default').css('background-color', 'transparent');
-              $('.nav-link, .nav-link__icon, .dropdown-toggle, .dropdown-toggle__icon, .caret').css('color', '#BDBDBD');
+              $('.nav-link, .nav-link__icon, .dropdown-toggle, .dropdown-toggle__icon, .caret.top').css('color', '#BDBDBD');
             }   
       });
     }
@@ -199,86 +199,4 @@ $('.carousel').carousel({
   pause: true,
 });
 
-//-------------------------------------//
-// init Masonry
 
-var $grid = $('.grid').masonry({
-  itemSelector: 'none', // select none at first
-  columnWidth: '.grid__col-sizer',
-  gutter: '.grid__gutter-sizer',
-  percentPosition: true,
-  stagger: 30,
-  // nicer reveal transition
-  visibleStyle: { transform: 'translateY(0)', opacity: 1 },
-  hiddenStyle: { transform: 'translateY(100px)', opacity: 0 },
-});
-
-// get Masonry instance
-var msnry = $grid.data('masonry');
-
-// initial items reveal
-$grid.imagesLoaded( function() {
-  $grid.removeClass('are-images-unloaded');
-  $grid.masonry( 'option', { itemSelector: '.grid__item' });
-  var $items = $grid.find('.grid__item');
-  $grid.masonry( 'appended', $items );
-});
-
-//-------------------------------------//
-// hack CodePen to load pens as pages
-
-var nextPenSlugs = [
-  'InfiniteScroll4.htm',
-  'InfiniteScroll2.htm',
-  'InfiniteScroll3.htm',
-  'InfiniteScroll4.htm',
-  'InfiniteScroll2.htm',
-  'InfiniteScroll3.htm',
-
-];
-
-function getPenPath() {
-  var slug = nextPenSlugs[ this.loadCount ];
-  if ( slug ) {
-    return 'https://github.com/spideyinf/spideyinf.github.io/tree/master/08-06-Project-1-Art-without-frame/images/htm/' + slug;
-  }
-}
-
-//-------------------------------------//
-// init Infinte Scroll
-
-$grid.infiniteScroll({
-  path: getPenPath,
-  append: '.grid__item',
-  outlayer: msnry,
-  scrollThresold: 200,
-  status: '.page-load-status'
-});
-
-//Add content to all HTML grid__item
-// $(function(){
-//   $('.grid__item').prepend(`
-//     <div class="products__main__products__info">
-//                 <div class="part-gray">
-//                   <div class="product-detail__info__options__tags">
-//                     <span style="font-size: .75rem;">Sơn dầu</span>
-//                     <span style="font-size: .75rem;">Trừu tượng</span>
-//                   </div>
-//                   <div class="summary">
-//                     <p>"The more effort you put into improving your skills, the bigger the payoff you will get. Realize that things will be hard at first, but the rewards will be worth it."</p>
-//                   </div>
-//                   <h5 class="price">VND 2,300,000</h5>
-//                   <div class="icon">
-//                     <i data-toggle="tooltip" data-placement="top" title="Thêm vào Yêu thích" class="fa fa-heart-o" aria-hidden="true"></i>
-//                     <i data-toggle="tooltip" data-placement="top" title="Thêm vào Giỏ hàng!" class="fa fa-cart-plus" aria-hidden="true"></i>
-//                     <i data-toggle="tooltip" data-placement="top" title="Tìm hiểu thêm" class="fa fa-search" aria-hidden="true"></i>
-//                   </div>
-//                 </div>
-//                 <div class="part-red">
-//                   <h6>the Inspiration</h6>
-//                   <p class="white">Tác giả: A Famous Artist</p>
-//                 </div>
-//               </div>
-//             </div>
-//     `);
-// })
