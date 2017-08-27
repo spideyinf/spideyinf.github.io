17c9182eceb785366cdb59f367288898b7bf1bc9
spideyinf <spidey.inf@gmail.com>
2017-08-28 01:09:25 +0700

    Let's go!

--- 08-06-Project-1-Art-without-frame/js/main.js
+++ 08-06-Project-1-Art-without-frame/js/main.js
@@ -199,4 +199,27 @@ $('.carousel').carousel({
   pause: true,
 });
 
+//Home Bestsellers Slider
+
+$(document).ready(function(){
+
+  $('#itemslider').carousel({ interval: 5000 });
+
+  $('.carousel-showmanymoveone .item').each(function(){
+    var itemToClone = $(this);
+
+    for (var i=1;i<6;i++) {
+    itemToClone = itemToClone.next();
+
+    if (!itemToClone.length) {
+    itemToClone = $(this).siblings(':first');
+    }
+
+    itemToClone.children(':first-child').clone()
+    .addClass("cloneditem-"+(i))
+    .appendTo($(this));
+    }
+  });
+});
+
 
